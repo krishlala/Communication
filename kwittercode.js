071e45d7db686d045fd4884d8dd41f795631374f
krishlala <71400038+krishlala@users.noreply.github.com>
2021-05-15 17:48:50 -0400

    Update kwittercode.js

--- kwittercode.js
+++ kwittercode.js
@@ -4,5 +4,8 @@ function addUser() {
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
     window.location = "kwitter_room.html";
+        
+        else {
+            console.log("You suck Krish! It didn't work!")
 }
 }
