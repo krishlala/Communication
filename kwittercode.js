d5156bd3b5927273c924b0919a07eab587ae73a7
krishlala <71400038+krishlala@users.noreply.github.com>
2021-05-15 20:57:34 -0400

    Update kwittercode.js

--- kwittercode.js
+++ kwittercode.js
@@ -1,7 +1,7 @@
 function addUser() {
     var password = "lala123";
     password2 = document.getElementById("password");
-    console.log("Password2="+password2);
+    console.log("Password2="+password2.value);
     if (password == password2) {
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
