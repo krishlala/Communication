ad67f7eed138938fd5894c1b69fa6f228d784fc1
krishlala <71400038+krishlala@users.noreply.github.com>
2021-05-17 16:23:14 -0400

    Update account.js

--- account.js
+++ account.js
@@ -1,5 +1,12 @@
 function addUser() {
-    localStorage.setItem("user_name", user_name).value;
-      localStorage.setItem("password", password).value;
-        window.location = "kwitter_room.html";
+
+    user_name = document.getElementById("user_name").value;
+
+    localStorage.setItem("user_name", user_name);
+    
+    password = document.getElementById("password").value;
+    
+    localStorage.setItem("password", password);
+
+    window.location = "kwitter_room.html"
 }
